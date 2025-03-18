1b649ff078ce91a7874ad34d37b1eb0e777a37f2
Diego Rodríguez <dieguitotoi@gmail.com>
2025-03-17 22:57:45 -0500

    npm install --save-dev webpack @types/webpack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,37 @@
 import type { NextConfig } from "next";
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-const nextConfig: NextConfig = {
-  output: "export", // Habilita la exportación estática
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = withBundleAnalyzer({
+  // output: "export", // Habilita la exportación estática
+  output: "standalone",
+  trailingSlash: true, // Asegura compatibilidad con rutas en S3 o Netlify
+  reactStrictMode: true, // Habilita el modo estricto de React para detectar errores
   images: {
-    unoptimized: true, // Evita la optimización de imágenes (necesario para S3)
+    unoptimized: true, // Desactiva optimización de imágenes (para S3, pero revisa si es necesario)
+    formats: ['image/avif', 'image/webp'], // 🔥 Next.js elegirá AVIF si el navegador lo soporta
+    minimumCacheTTL: 60, // 🔥 Cachea imágenes por 60s mínimo en el servidor
   },
-  webpack(config) {
-    config.module.rules.push({
+  experimental: {
+    scrollRestoration: true, // Mejora la experiencia de usuario evitando recargas innecesarias
+    outputFileTracingIncludes: {
+      "/*": ["node_modules/some-large-lib/**"], // Evita incluir paquetes innecesarios
+    },
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production", // Elimina `console.log` en producción
+  },
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"], // Permite importar SVGs como componentes
     });
+
     return config;
   },
-};
+});
 
 export default nextConfig;
